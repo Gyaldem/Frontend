ca3f9fd84d757f5045bb56ba553bfb2a3fa5d674
Amina <la_malek@esi.dz>
2024-02-10 07:35:06 +0100

    fenetre confirm submission

--- my-project/src/components/fenetreSub.jsx
+++ my-project/src/components/fenetreSub.jsx
@@ -4,10 +4,12 @@ import Check from '../img/Check.svg';
 
 function FenetreSub() {
     return (
-        <div>
-            <img src={Cross} alt="Cross" />
-            <span> Confirm your submission </span>
-            <img src={Check} alt="Check" />
+        <div className="flex flex-col w-1/3 py-5 space-y-6 border rounded-2xl">
+            <div><img src={Cross} alt="Cross" className="ml-auto mr-3" /> </div>
+            <div className="flex flex-col items-center space-y-6" >
+                <span className=""> Confirm your submission </span>
+                <div> <img src={Check} alt="Check" className="px-8 py-1 rounded-2xl bg-second" /> </div>
+            </div>
         </div>
 
     )
